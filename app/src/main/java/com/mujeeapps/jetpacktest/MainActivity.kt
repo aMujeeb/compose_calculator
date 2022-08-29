62c1d2bf99720320ca1be6e9e4b5ec04c701245b
Mujee <mujee2@gmail.com>
2022-08-29 23:49:00 +0800

    Update MainActivity.kt
    2022/08/29

--- app/src/main/java/com/mujeeapps/jetpacktest/MainActivity.kt
+++ app/src/main/java/com/mujeeapps/jetpacktest/MainActivity.kt
@@ -3,9 +3,12 @@ package com.mujeeapps.jetpacktest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -14,12 +17,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,57 +34,86 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.screen3)
-            val description = "Coconut Girl"
-            val title = "Coconut Tree"
-            Box(modifier = Modifier.fillMaxWidth(fraction = 0.5f)
-                    .padding(16.dp)) {
-                CardDetailsView(painter = painter, description = description, title = title)
+            MyTipApp {
+                Column(horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Top) {
+                    TopHeader()
+                    MainContent()
+                }
             }
         }
     }
 }
 
 @Composable
-fun CardDetailsView(
-        painter: Painter, //Painter for UI property access
-        description: String,
-        title: String,
-        modifier: Modifier = Modifier
-) {
-    Card(
-            modifier = modifier.fillMaxWidth(),
-            elevation = 4.dp,
-            shape = RoundedCornerShape(16.dp)
+fun MyTipApp(content: @Composable () -> Unit) {
+    JetPackTestTheme {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(), color = Color.White
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun TopHeader(totalPerPerson: Double = 0.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(8.dp)
+            //.clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))
+            .clip(
+                shape = CircleShape.copy(all = CornerSize(12.dp))
+            ),
+        color = Color(0xFFE9D7F7)
     ) {
-        //Box modifier stacks controllers one top of other
-        Box(modifier = Modifier
-                .height(200.dp)
-                .fillMaxWidth(fraction = 1f)) {
-            Image(painter = painter,
-                    contentDescription = description,
-                    contentScale = ContentScale.FillBounds
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.h5
             )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
 
-            Box(modifier = Modifier.fillMaxSize()
-                    .background(brush =
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                    Color.Transparent,
-                                    Color.Black
-                            ), startY = 400f
-                    ))
-            ) {
+@Preview(showBackground = true)
+@Composable
+fun MainContent() {
+    Surface(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(), shape = RoundedCornerShape(
+            corner = CornerSize(12.dp)
+        ), border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column() {
+            Text(text = "Test 1")
+            Text(text = "Test 2")
+            Text(text = "Test 3")
+        }
+    }
+}
 
-            }
 
-            Box(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                    contentAlignment = Alignment.BottomStart
-            ) {
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-            }
+@Composable
+fun DefaultPreview() {
+    JetPackTestTheme {
+        MyTipApp {
+            TopHeader()
         }
     }
 }
